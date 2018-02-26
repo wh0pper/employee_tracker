e10450eee6730d60d6cce1b3ef30c6af0ada6b24
Anna Marie Cooper and Bryce Frazier <bryce@>
2018-02-26 11:38:30 -0800

    add views and forms

--- app.rb
+++ app.rb
@@ -9,3 +9,35 @@ require("pg")
 get("/") do
   erb(:index)
 end
+
+get("/divisions") do
+  @divisions = Division.all()
+  erb(:divisions)
+end
+
+get("/employees") do
+  @employees = Employee.all()
+  erb(:employees)
+end
+
+post("/division") do
+  title = params.fetch("title")
+  division = Division.new({:title => title, :id => nil})
+  division.save()
+  erb(:success)
+end
+
+get("/division/new") do
+  erb(:divisionform)
+end
+
+get("/employee/new") do
+  erb(:employeeform)
+end
+
+post("/employee") do
+  name = params.fetch("name")
+  employee = Employee.new({:name => name, :id => nil})
+  employee.save()
+  erb(:success)
+end
